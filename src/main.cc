6e6f46d4f25771d379bb242a336371e3a622f9c9
d1shs0ap <42086645+d1shs0ap@users.noreply.github.com>
2020-12-08 04:15:06 -0500

    changed main to text controller

--- src/main.cc
+++ src/main.cc
@@ -3,50 +3,65 @@
 #include <vector>
 
 #include "game/game.h"
-#include "util.h"
+#include "view/textDisplay.h"
+#include "controller/textController.h"
 
 bool TESTING = false;
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     Game game;
+    TextDisplay textDisplay;
+    shared_ptr<TextController> textController;
+
     bool supplied1 = false;
     bool supplied2 = false;
-    std::string cmd;
-    while (std::cin >> cmd) {
-        if (cmd == "help")
-        {
-            displayHelp();
-        }
-        else if (cmd == "end")
-        {
-            game.endTurn();
-        }
-        else if (cmd == "quit")
-        {
-            std::cout << quitMessage << std::endl;
-            return 0;
-        }
-        else if (cmd == "draw")
-        {
-            if (TESTING == false)
-                std::cout << "Command draw is only avaliable in testing mode." << std::endl;
-            else
-            {
-                // note: got some problem here.
+
+    // for storing the commands
+    string cmd;
+    try { // Is the try catch correct?
+        while (cin >> cmd) {
+            if (cmd == "help") {
+                textController->help();
             }
-            // note: recall that a player can only do this when the number of
-            //       cards in hand is less than 5. This is checked within the
-            //       function Player::draw.
-        }
-        else if (cmd == "discard")
-        {
-            if (TESTING == false)
-                std::cout << "Command draw is only avaliable in testing mode." << std::endl;
-            else
-            {
-                // note: got some problem here.
+            else if (cmd == "end") {
+                textController->end();
+            }
+            else if (cmd == "quit") {
+                textController->quit();
+            } else if (cmd == "draw") {
+                if (TESTING == false)
+                    cout << "Command draw is only avaliable in testing mode." << endl;
+                else {
+                    textController->draw();
+                }
+                // note: recall that a player can only do this when the number of
+                //       cards in hand is less than 5. This is checked within the
+                //       function Player::draw.
+            } else if (cmd == "discard") {
+                if (TESTING == false)
+                    cout << "Command discard is only avaliable in testing mode." << endl;
+                else {
+                    textController->discard();
+                }
+            } else if (cmd == "attack") {
+                int minion;
+                cin >> minion;
+                
+
+            } else if (cmd == "play") {
+
+            } else if (cmd == "use") {
+
+            } else if (cmd == "inspect") {
+                textController->inspect();
+            } else if (cmd == "hand") {
+                textController->hand();
+            } else if (cmd == "board") {
+                textController->board();
             }
         }
-    }
+    } catch (ios::failure &) {}
 }
