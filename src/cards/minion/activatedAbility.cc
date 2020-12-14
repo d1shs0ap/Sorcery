dbd0820d5c22e52ea8334e4adfc76272b54d04cf
Jasonwu-0803 <65523703+Jasonwu-0803@users.noreply.github.com>
2020-12-14 16:15:08 +0800

    virtual

--- src/cards/minion/activatedAbility.cc
+++ src/cards/minion/activatedAbility.cc
@@ -1,5 +1,7 @@
 #include "activatedAbility.h"
 #include "minion.h"
+#include "../../game/game.h"
+#include "../card.h"
 
 ActivatedAbility::ActivatedAbility() {}
 
@@ -9,5 +11,12 @@ ActivatedAbility::ActivatedAbility(std::string description, int cost, std::share
 int ActivatedAbility::getCost() const { return cost; }
 
 std::string ActivatedAbility::getDescription() const { return description; }
+
+void ActivatedAbility::effect(Game game) const{
+
+}
+void ActivatedAbility::effect(Card card) const{
+
+}
 
 
